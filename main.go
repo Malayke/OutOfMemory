564ab70f338107cdcceab45557e50278e7a306bd
Malayke <Malayke@users.noreply.github.com>
2023-12-11 13:10:22 +0800

    update code

--- main.go
+++ main.go
@@ -93,27 +93,6 @@ func OutOfMemoryGood4(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
-func OutOfMemoryGood5(w http.ResponseWriter, r *http.Request) {
-	source := r.URL.Query()
-	MaxValue := 6
-	sourceStr := source.Get("n")
-	sink, err := strconv.Atoi(sourceStr)
-	if err != nil || sink < 0 {
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
-	}
-	if sink < 0 || sink > MaxValue {
-		sink = MaxValue
-	}
-	result := make([]string, sink)
-	for i := 0; i < sink; i++ {
-		result[i] = fmt.Sprintf("Item %d", i+1)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(result)
-}
-
 func OutOfMemoryBad(w http.ResponseWriter, r *http.Request) {
 	source := r.URL.Query()
 
@@ -139,7 +118,6 @@ func main() {
 	http.HandleFunc("/oom_good2", OutOfMemoryGood2)
 	http.HandleFunc("/oom_good3", OutOfMemoryGood3)
 	http.HandleFunc("/oom_good4", OutOfMemoryGood4)
-	http.HandleFunc("/oom_good5", OutOfMemoryGood5)
 
 	fmt.Println("Starting server on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
